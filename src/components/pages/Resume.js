ea97e21ba795ce4a64c5b288ad896b872068f15a
Elaine Martinez <elainefmartinez6@gmail.com>
2023-10-02 13:15:00 -0700

    updated proficiencies and style for resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,36 @@
 import React from "react";
 import file from '../../assets/Resume.pdf';
+import './Resume.css';
 
 const Resume = () => {
     return (
-        <div className="pdflink">
-            <h1>TEST</h1>
-            <a className="newre" href ={file}>View Full Resume</a>
+        <div className="resume">
+            <h1>Proficiencies</h1>
+                <li>
+                    HTML
+                </li>
+                <li>
+                    CSS
+                </li>
+                <li>
+                    JavaScript
+                </li>
+                <li>
+                    Git
+                </li>
+                <li>
+                    CSS
+                </li>
+                <li>
+                    Node.js
+                </li>
+                <li>
+                    Express.js
+                </li>
+                <li>
+                    React
+                </li>
+            <a className="resumelink" href ={file}>View Full Resume</a>
         </div>
     );
 }
